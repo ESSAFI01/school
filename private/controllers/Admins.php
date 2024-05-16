fc7635257e0b2e2e84210f5f463f1804e25a10ab
ESSAFI01 <essafianas68@gmail.com>
2024-05-16 16:12:49 +0100

    wkoe

--- private/controllers/Admins.php
+++ private/controllers/Admins.php
@@ -4,11 +4,6 @@ class Admins extends Controller
 {
     function index()
 	{
-<<<<<<< HEAD
-        
-=======
-     
->>>>>>> de73af56ebf26c9c19dd7fba031c1a6bd16405fc
 		
 		if(!Auth::adminLoggedIn())
 		{
